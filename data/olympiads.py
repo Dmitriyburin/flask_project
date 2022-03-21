88435ab64386657d07c69d43b83f800690d5f0ed
sovun1a <burindima324@gmail.com>
2022-03-21 19:14:52 +0300

    add subject and stage

--- data/olympiads.py
+++ data/olympiads.py
@@ -5,6 +5,8 @@ from sqlalchemy.orm import relationship
 from sqlalchemy_serializer import SerializerMixin
 
 from .db_session import SqlAlchemyBase
+from .olympiads_to_subjects import Subjects
+from .olympiads_to_stages import olympiads_to_stages_table
 
 
 class Olympiads(SqlAlchemyBase, SerializerMixin):
@@ -12,7 +14,6 @@ class Olympiads(SqlAlchemyBase, SerializerMixin):
 
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
 
-    # subject_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("subjects.id"), nullable=True)
     subjects = relationship("Subjects",
                             secondary="olympiads_to_subjects",
                             back_populates="olympiads")
@@ -25,8 +26,27 @@ class Olympiads(SqlAlchemyBase, SerializerMixin):
     description = sqlalchemy.Column(sqlalchemy.Text)
     duration = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
     link = sqlalchemy.Column(sqlalchemy.String(150), nullable=True)
-    # date = sqlalchemy.Column(sqlalchemy.String(35), nullable=True, default=datetime.date.today())
-
     stages = relationship("Stages",
                           secondary="olympiads_to_stages",
                           back_populates="olympiads")
+
+    users = relationship("Users",
+                         secondary="users_to_olympiads",
+                         back_populates="olympiads")
+
+    def add_subject(self, session, subject_id):
+        subject = session.query(Subjects).get(subject_id)
+        self.subjects.append(subject)
+        session.commit()
+
+    def add_stage(self, session, stage_id, date):
+        stage = olympiads_to_stages_table.insert().values(olympiads_id=self.id,
+                                                          stages_id=stage_id,
+                                                          date=date)
+        session.execute(stage)
+        session.commit()
+    #
+    # def delete_subject(self, session, subject_id):
+    #     subject = session.query(Subjects).get(subject_id)
+    #     self.subjects.de(subject)
+    #     session.commit()

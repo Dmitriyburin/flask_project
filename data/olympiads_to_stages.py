4c8465f047c2372e9cc5df40b31f4b255e3f4d21
sovun1a <burindima324@gmail.com>
2022-03-26 16:33:50 +0300

    change stages_table

--- data/olympiads_to_stages.py
+++ data/olympiads_to_stages.py
@@ -3,17 +3,17 @@ import sqlalchemy
 from sqlalchemy.orm import relationship
 
 from .db_session import SqlAlchemyBase
-
-olympiads_to_stages = sqlalchemy.Table('olympiads_to_stages', SqlAlchemyBase.metadata,
-                                             sqlalchemy.Column('olympiads_id', sqlalchemy.Integer,
-                                                               sqlalchemy.ForeignKey('olympiads_table.id'),
-                                                               primary_key=True, ),
-                                             sqlalchemy.Column('stages_id', sqlalchemy.Integer,
-                                                               sqlalchemy.ForeignKey('stages_table.id'),
-                                                               primary_key=True, ),
-                                             sqlalchemy.Column('date', sqlalchemy.Date, nullable=True,
-                                                               default=datetime.date.today())
-                                             )
+#
+# olympiads_to_stages = sqlalchemy.Table('olympiads_to_stages', SqlAlchemyBase.metadata,
+#                                              sqlalchemy.Column('olympiads_id', sqlalchemy.Integer,
+#                                                                sqlalchemy.ForeignKey('olympiads_table.id'),
+#                                                                primary_key=True, ),
+#                                              sqlalchemy.Column('stages_id', sqlalchemy.Integer,
+#                                                                sqlalchemy.ForeignKey('stages_table.id'),
+#                                                                primary_key=True, ),
+#                                              sqlalchemy.Column('date', sqlalchemy.Date, nullable=True,
+#                                                                default=datetime.date.today())
+#                                              )
 
 
 class Stages(SqlAlchemyBase):
@@ -21,8 +21,10 @@ class Stages(SqlAlchemyBase):
 
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
     name = sqlalchemy.Column(sqlalchemy.String(35), nullable=True)
-    olympiad_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('olympiads.id'))
+    olympiad_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('olympiads_table.id'))
     olympiad = relationship('Olympiads', back_populates="stages")
+    date = sqlalchemy.Column(sqlalchemy.Date, default=datetime.date.today())
+    description = sqlalchemy.Column(sqlalchemy.Text, nullable=True)
     # olympiads = relationship("Olympiads",
     #                          secondary="olympiads_to_stages",
     #                          back_populates="stages")

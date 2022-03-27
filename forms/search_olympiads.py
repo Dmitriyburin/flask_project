3421c16e2cc760a43769c770915150fde22a9c9e
sovun1a <burindima324@gmail.com>
2022-03-27 12:15:21 +0300

    filter title olympiad

--- forms/search_olympiads.py
+++ forms/search_olympiads.py
@@ -1,9 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, IntegerField, BooleanField, SelectField
+from wtforms import StringField, SubmitField, SearchField, BooleanField, SelectField
 from wtforms.validators import DataRequired
 
 
 class SearchOlympiadForm(FlaskForm):
     subject = SelectField('Предмет', coerce=str)
     school_class = SelectField('Школьный класс')
+    title = SearchField('Название')
     submit = SubmitField('Submit')

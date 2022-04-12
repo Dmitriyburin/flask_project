8d65b93a5e62de67da4f4f6eb244cf4acb1cb683
sovun1a <burindima324@gmail.com>
2022-04-12 17:11:38 +0300

    add datepicker

--- forms/search_olympiads.py
+++ forms/search_olympiads.py
@@ -1,10 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, SearchField, BooleanField, SelectField
+from wtforms import StringField, SubmitField, SearchField, DateField, SelectField
 from wtforms.validators import DataRequired
+import datetime
 
 
 class SearchOlympiadForm(FlaskForm):
     subject = SelectField('Предмет', coerce=str)
     school_class = SelectField('Школьный класс')
     title = SearchField('Название')
+    date = DateField('Дата', default=datetime.datetime.now)
     submit = SubmitField('Submit')
